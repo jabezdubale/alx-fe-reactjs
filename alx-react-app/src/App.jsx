9a6a89bbbab45a9431960b2eec9f3bfad2327d21
jabezdubale <jabezdubale6@gmail.com>
2025-07-11 05:17:17 +0300

    3. Create a User Profile Card Using Props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,7 +6,7 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
-import UserProfile from "./components/UserProfile ";
+import UserProfile from "./UserProfile ";
 
 function App() {
   const [count, setCount] = useState(0);
