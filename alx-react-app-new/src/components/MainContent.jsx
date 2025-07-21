859375070e5e320bb0039a53291dad089b83de02
jabezdubale <jabezdubale6@gmail.com>
2025-07-21 11:48:53 +0300

    0. Apply Inline CSS Styling to React Components

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,10 +1,7 @@
 function MainContent() {
   return (
     <main style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
-      <p>
-        I love to visit{" "}
-        <span style={{ fontWeight: "bold" }}> New York, Paris, and Tokyo.</span>
-      </p>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
