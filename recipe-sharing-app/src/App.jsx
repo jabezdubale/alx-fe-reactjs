8169063d12aa2d3239aa4ee790b8847e2945ca33
jabezdubale <jabezdubale6@gmail.com>
2025-07-29 06:32:26 +0300

    0. Initialize the Recipe Sharing Application with Zustand State Management

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+
+function App() {
+  return (
+    <>
+      <RecipeList />
+      <AddRecipeForm />
+    </>
+  );
+}
+
+export default App;
