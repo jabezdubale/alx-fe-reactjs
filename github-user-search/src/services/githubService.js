0c0689aca81204d3177ef3d90e0cc619fecc5eef
jabezdubale <jabezdubale6@gmail.com>
2025-08-02 08:35:59 +0300

    2. Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -4,7 +4,6 @@ export const fetchUserData = async (userName, location, minRepos) => {
   const url = `https://api.github.com/search/users?q=${userName}+location:${location}+repos:>=${minRepos}`;
   try {
     const res = await axios.get(url);
-    console.log(url);
     return res.data;
   } catch (err) {
     console.error("There is an error fetching user: ", err);
