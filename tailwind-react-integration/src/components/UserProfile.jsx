0b6b65d725385f43a0f9efceabb9085139450e1e
jabezdubale <jabezdubale6@gmail.com>
2025-08-10 15:04:46 +0300

    1. Styling a Provided React Component with Tailwind CSS

---
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -0,0 +1,22 @@
+function UserProfile() {
+  return (
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+      <img
+        src="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D"
+        alt="User"
+        className="rounded-full w-36 h-36 mx-auto"
+      />
+      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
+      <p className="text-gray-600 text-base">
+        Developer at Example Co. Loves to write code and explore new
+        technologies.
+      </p>
+    </div>
+  );
+}
+
+export default UserProfile;
+
+// Paragraph (p):
+// Set the text color to gray-600 (text-gray-600).
+// Make the font size slightly smaller than the heading (text-base).
