cce1226f8ec4804239acb0251ca337c455c25a44
jabezdubale <jabezdubale6@gmail.com>
2025-08-15 11:39:22 +0300

    2. Building the Recipe Detail Page

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,15 +1,26 @@
 import { useParams } from "react-router-dom";
 import datas from "../data.json";
+import { useEffect, useState } from "react";
 
 const RecipeDetail = () => {
   const { id } = useParams();
-  const data = datas.find((item) => item.id === Number(id));
+  const [data, setData] = useState([]);
+  const [ingredients, setIngredients] = useState("");
+  const [instructions, setInstructions] = useState("");
+  useEffect(() => {
+    setData(datas.find((item) => item.id === Number(id)));
+  }, [id]);
+
+  useEffect(() => {
+    setIngredients(data.summary);
+    setInstructions(data.Detail);
+  }, [data]);
 
   return (
     <div className="w-3/5 bg-gray-100 mx-auto p-5 my-5 rounded-3xl">
       <h2 className="text-4xl mt-3 mx-auto">{data.title}</h2>
-      <p className="text-2xl font-light">{data.summary}</p>
-      <p className="text-base text-justify mx-3 p-3">{data.Detail}</p>
+      <p className="text-2xl font-light">{ingredients}</p>
+      <p className="text-base text-justify mx-3 p-3">{instructions}</p>
       <img
         src={data.image}
         alt="food image"
