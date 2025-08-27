656bff3f0e7d60bd132b6cbe9218e4a2b2453c1e
jabezdubale <jabezdubale6@gmail.com>
2025-08-27 11:44:39 +0300

    2. Advanced Routing in React with React Router

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -2,17 +2,17 @@ import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [useAuth, setUseAuth] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
 
-  if (!isAuthenticated) {
+  if (!useAuth) {
     return (
       <div>
         <h3>This is a protected Route space</h3>
         <button
           onClick={() => {
-            setIsAuthenticated(true);
+            setUseAuth(true);
             navigate(location.pathname, { replace: true });
           }}
         >
